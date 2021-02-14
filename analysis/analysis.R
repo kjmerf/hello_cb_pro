e1a51786b341551c2c8e221f5e0327e10e13bafe
baogorek <baogorek@gmail.com>
2021-02-14 11:59:12 -0600

    Kalman Filter iteration 1

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(lubridate)
 library(qcc)
+library(urca)
+library(forecast)
 
 df <- read.csv('/tmp/bitcoin.csv')
 df$hour <- hour(ymd_hms(df$iso_time))
@@ -10,16 +12,26 @@ daily <- df %>% filter(hour == 17)
 plot(close ~ date, data = daily)
 T <- nrow(daily)
 
+auto.arima(log(daily$close))
+
+
 log_diffs <- log(daily$close[2:T]) - log(daily$close[1:(T-1)])
 plot(log_diffs)
+acf(log_diffs)
 
 
-diff_df <- data.frame(t = 1:(T-1), log_diff = log_diffs)
+
+diff_df <- data.frame(dt = daily$date[2:T], t = 2:T, log_diff = log_diffs)
+summary(ur.df(y=diff_df$log_diff, type = "drift", lags=1))
+
+
+
 
 # Try taking out the 4 largest points
 diff_df %>% arrange(desc(abs(log_diff))) %>% head()
-#diff_df <- diff_df %>% filter(!(t %in% c(142, 143, 195, 177)))
+#diff_df <- diff_df %>% filter(!(t %in% c(143, 144, 196, 178)))
 
+auto.arima(diff_df$log_diff)
 qcc(diff_df$log_diff, type = 'xbar.one')
 
 mean(diff_df$log_diff)
@@ -36,5 +48,9 @@ diff_df$var_hat <- predict(loess_var)
 
 plot(sq_dev ~ t, data = diff_df)
 points(var_hat ~ t, data = diff_df, cex=.5, col = 'blue')
+
+plot(level_hat ~ dt, data = diff_df)
+
+
 
 
--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,3 +1,56 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import statsmodels as sm
+from statsmodels.tsa.statespace.mlemodel import MLEModel
+
+
+# Read in data
 df = pd.read_json("/tmp/cb_pro.json", lines=True)
-df.to_csv("/tmp/bitcoin.csv", index=True)
+df.to_csv("/tmp/bitcoin.csv", index=False)
+
+# Process with analysis.R and come back
+daily = pd.read_csv("/tmp/daily-bitcoin.csv")
+
+class MomentumFilter(MLEModel):
+    param_names = ['sigma_a', 'sigma_e']
+    start_params = [.001, .0011]
+
+    def __init__(self, logprice):
+        initial = logprice[0]
+        super().__init__(logprice[1:], k_states=2)
+
+        self.initialize_known(np.array([initial, 0]), np.eye(2))
+
+    def update(self, params, **kwargs):
+        params = super().update(params, **kwargs)
+        
+        self['transition'] = np.array([[1, 1], [0, 1]])
+
+        # premultiplied by state vector
+        self['design'] = np.array([1, 0])
+
+        # premultiplied by state error
+        self['selection'] = np.diag([1, 1])
+
+        self['state_cov'] = np.diag([params[1] ** 2, params[0] ** 2])
+
+        self['obs_intercept', 0, 0] = 0
+        self['obs_cov', 0, 0] = 0
+
+logp = np.log(daily['close'])
+
+my_filter = MomentumFilter(np.log(daily['close']))
+my_fit = my_filter.fit(method='lbfgs', bounds = ((0, 1), (0, 1)))
+my_fit.summary()
+
+plt.plot(logp[1:])
+plt.plot(my_fit.fittedvalues)
+
+# This should just be the logprice
+plt.plot(my_fit.filtered_state[0, :])
+plt.show()
+
+# This is the drift term
+plt.plot(my_fit.filtered_state[1, :])
