9ea585c06e3d12cbfd132ba23fa7a9b52d22dfeb
Kevin Merfeld <kevin.merfeld@zoro.com>
2021-02-19 17:41:37 -0600

    fixing SQL formatting

--- src/app/sql/load_ods_candles.sql
+++ src/app/sql/load_ods_candles.sql
@@ -22,13 +22,8 @@ select l.product_id
   , l.created_time
 from (
 	select *
-		, row_number() over (
-			partition by product_id
-			, time order by created_time desc
-			) as rn
+		, row_number() over (partition by product_id, time order by created_time desc) as rn
 	from landing.candles
 	) l
-left join ods.candles o on l.product_id = o.product_id
-	and l.time = o.time
-where o.product_id is null
-and l.rn = 1;
+left join ods.candles o on l.product_id = o.product_id and l.time = o.time
+where o.product_id is null and l.rn = 1;
